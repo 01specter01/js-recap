373c84127a13b02a71c3ee15cabde13c5540dd65
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-31 13:05:34 +0200

    2 done

--- solution.js
+++ solution.js
@@ -13,3 +13,23 @@ console.log(canNest([1, 2, 3, 4], [0, 6]));
 console.log(canNest([3, 1], [4, 0]));
 console.log(canNest([9, 9, 8], [8, 9]));
 console.log(canNest([1, 2, 3, 4], [2, 3]));
+console.log();
+console.log("Aufgabe 2");
+
+class Kreis {
+    constructor(r) {
+        this.r = r;
+    }
+
+    getArea() {
+        return Math.PI * Math.pow(this.r, 2);
+    }
+    getPerimeter() {
+        return 2 * Math.PI * this.r;
+    }
+}
+let kreis1 = new Kreis(11);
+console.log(kreis1.getArea()); // Should return 380.132711084365
+
+let kreis = new Kreis(4.44);
+console.log(kreis.getPerimeter()); // Should return 27.897342763877365
