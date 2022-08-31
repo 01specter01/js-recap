48fdeaeb2ae9be5c8ab197b36caefb53c05f155d
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-31 11:01:17 +0200

    s

--- solution.js
+++ solution.js
@@ -3,9 +3,11 @@ function canNest(arr1, arr2) {
     if (
         Math.max(...arr1) < Math.max(...arr2) &&
         Math.min(...arr1) > Math.min(...arr2)
-    )
+    ) {
         return true;
-    else return false;
+    } else {
+        return false;
+    }
 }
 console.log(canNest([1, 2, 3, 4], [0, 6]));
 console.log(canNest([3, 1], [4, 0]));
